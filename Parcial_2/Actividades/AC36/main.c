b5271fad7b0d43bc2a0ae87412250591c7aed874
QuickFixDev <juan.hernandez.mirabile@gmail.com>
2024-10-18 16:47:32 -0600

    add sucesion con banderas

---
+++ Parcial_2/Actividades/AC36/main.c
@@ -0,0 +1,28 @@
+Pseudocódigo
+"Sucesion con banderas"
+
+INICIO
+    DVyC
+        entero n, valorSerie, suma
+        booleano bandera
+    DE
+        repetir
+            imprimir "Introduce un valor para n (n >= 10): "
+            leer n
+        mientras(n < 10)
+
+    PROCESO
+        valorSerie = 2
+        bandera = verdadero  // Inicia en verdadero para sumar 3 primero
+        mientras (valorSerie <= n)
+            imprimir valorSerie
+            si (bandera == verdadero)
+                suma = 3
+                bandera = falso
+            sino
+                suma = 2
+                bandera = verdadero
+            fin si
+            valorSerie = valorSerie + suma
+    fin mientras
+FIN
